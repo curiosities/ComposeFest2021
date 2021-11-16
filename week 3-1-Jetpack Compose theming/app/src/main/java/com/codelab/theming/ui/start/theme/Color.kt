326bdea298925e50d9c3f2eb20760f5ed5e584f0
Dexter Gim <curiosittyy@gmail.com>
2021-11-17 00:04:33 +0900

    Compose theming

---
+++ week 3-1-Jetpack Compose theming/app/src/main/java/com/codelab/theming/ui/start/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.codelab.theming.ui.start.theme
+
+import androidx.compose.material.lightColors
+import androidx.compose.ui.graphics.Color
+
+/*
+When defining colors, we name them "literally", based on the color value, rather than "semantically" e.g.
+- Red500 not primary.
+ */
+val Red200 = Color(0xfff297a2)
+val Red300 = Color(0xffea6d7e)
+val Red700 = Color(0xffdd0d3c)
+val Red800 = Color(0xffd00036)
+val Red900 = Color(0xffc20029)
